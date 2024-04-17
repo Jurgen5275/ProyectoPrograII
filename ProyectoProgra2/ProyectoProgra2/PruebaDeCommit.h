5fbe70494ec1c30eda311754537af00e800ca23f
MarianaVillalobos04 <mariana.villalobos.ramirez@est.una.ac.cr>
2024-04-16 20:14:36 -0600

    prueba

---
+++ ProyectoProgra2/ProyectoProgra2/PruebaDeCommit.h
@@ -0,0 +1,17 @@
+#pragma once
+//prueba, copia de class Fecha
+
+class Fecha {
+private:
+	int dia;
+	int mes;
+	int anio;
+
+public:
+	Fecha() {}
+
+	Fecha(int d, int m, int an) {}
+
+	virtual ~Fecha() {}
+
+};
