56849ab0fbb0c8d7a46d5b7852ef3d980a0f5ecf
Jurgen5275 <153567327+Jurgen5275@users.noreply.github.com>
2024-04-19 19:56:53 -0600

    Delete ProyectoProgra2 directory

--- ProyectoProgra2/ProyectoProgra2/Embutido.cpp
+++
@@ -1,9 +0,0 @@
-#include "Embutido.h"
-#include <sstream>
-#include <iostream>
-using namespace std;
-
-Embutido::Embutido(string cod, string nom, string descrip, float prec, int exis, Categoria cate, Fecha fechV, bool nac, double pes, string animalNom, string partAnimal, bool tri, string marc)
-	: Carne(cod, nom, descrip, prec, exis, cate, fechV, nac, pes, animalNom, partAnimal), Empaque{ tri }, marca{ marc } {}
-
-Embutido::~Embutido() {}
--- ProyectoProgra2/ProyectoProgra2/Empaque.cpp
+++
@@ -1,8 +0,0 @@
-#include "Empaque.h"
-#include <sstream>
-#include <iostream>
-using namespace std;
-
-Empaque::Empaque(bool tri){}
-
-Empaque::~Empaque(){}
